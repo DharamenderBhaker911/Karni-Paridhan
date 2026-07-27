f876efbcf2f7698669a92d12526d93e4e82d10db
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-07-27 21:31:17 +0530

    Add Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import { Toaster } from 'react-hot-toast'
 import './index.css'
 import './pages.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Analytics />
       <App />
       <Toaster position="bottom-center" />
     </QueryClientProvider>
